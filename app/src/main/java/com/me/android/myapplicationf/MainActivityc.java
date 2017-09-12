c177911b1b982f5fc02165f8e5ade5922a60218c
dzh <1137744273@qq.com>
2017-09-12 21:10:59 +0800

    Initial commit

---
+++ app/src/main/java/com/me/android/myapplicationf/MainActivityc.java
@@ -0,0 +1,13 @@
+package com.me.android.myapplicationf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activityc);
+    }
+}
